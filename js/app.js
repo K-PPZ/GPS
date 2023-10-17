52e9a776438e7e54211a83463b5df382b1abe877
K-PPZ <kpapazian@edenschool.fr>
2023-10-17 15:03:22 +0200

    jsp

--- js/app.js
+++ js/app.js
@@ -1,17 +1,11 @@
-const form = document.querySelector("form");
-const postal = document.getElementById("code");
-const adresse = document.getElementById("adresse");
-
-form.addEventListener("submit",async (e) => {
-  e.preventDefault();
-  const res = (await( await fetch(`https://api-adresse.data.gouv.fr/search/?q=${adresse.value}&postcode=${postal.value}`)).json()).features[0].geometry.coordinates.reverse();
-  createMap(res);
-});
+let form = document.querySelector("form");
+let postal = document.getElementById("code");
+let adresse = document.getElementById("adresse");
 
 // Fonction d'initialisation de la carte
 function createMap(coor) {
     // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
-    const macarte = L.map('map').setView(coor, 11);
+    let macarte = L.map('map').setView(coor, 11);
     // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
     
     L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
@@ -23,3 +17,10 @@ function createMap(coor) {
     
     L.marker(coor).addTo(macarte);
 }
+
+
+form.addEventListener("submit",async (e) => {
+  e.preventDefault();
+  let res = (await( await fetch(`https://api-adresse.data.gouv.fr/search/?q=${adresse.value}&postcode=${postal.value}`)).json()).features[0].geometry.coordinates.reverse();
+  createMap(res);
+});
